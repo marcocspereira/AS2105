47e3353ca72fddb31a3df5903b8bd08160e2f244
gsilvap <gsp@student.dei.uc.pt>
2015-03-26 01:58:10 +0000

    login feito

---
+++ Projeto2/OrderWebApp/src/java/as/model/CMD.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package as.model;
+
+/**
+ *
+ * @author GonçaloSilva
+ */
+public class CMD {
+	public static final int UNIT = 1;
+	
+	public static final int ROOT = 1;
+
+	public static final int OK = 1;
+	public static final int ERROR = -1;
+	
+	public static final String usersTable = "users";
+	public static final String ordersTable = "orders";
+        
+	public static final String seedsTable = "seeds";
+	public static final String shrubsTable = "shrubs";
+	public static final String treesTable = "trees";
+}
